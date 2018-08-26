a63fbcb8476c554d1a8debf730cdd1cb9fdf6e32
Bruno <brunoesperancim@hotmail.com>
2018-08-25 22:08:27 -0300

    Aula 1

---
+++ src/main/java/br/com/listavip/Configuracao.java
@@ -0,0 +1,23 @@
+package br.com.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Configuracao {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String ola() {
+		return "Olá, bem vindo ao sistema lista VIPs";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Configuracao.class, args);
+	}
+
+}
