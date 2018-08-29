7f84bcabf285c93c99026b9d69c6ce737f9a8922
Bruno <brunoesperancim@hotmail.com>
2018-08-28 22:15:58 -0300

    Criando application.properties

--- src/main/java/br/com/listavip/Configuracao.java
+++ src/main/java/br/com/listavip/Configuracao.java
@@ -1,11 +1,13 @@
 package br.com.listavip;
 
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Configuracao {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(Configuracao.class, args);
 	}
 
