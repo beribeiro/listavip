aa0b11b31db2fda06d0f53c71896b31a1a203129
Bruno <brunoesperancim@hotmail.com>
2018-09-04 19:59:44 -0300

    Aula 2 pt 2 (Utilizei h2 database ao inves de MySQL)

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into TEST(NOME,EMAIL,TELEFONE) values('Bruno','brunoesperancim@hotmail.com','4975-7508');
+insert into TEST(NOME,EMAIL,TELEFONE) values('Bianca','asdfasdfm@hotmail.com','4523-7508');
+insert into TEST(NOME,EMAIL,TELEFONE) values('Claudio','claudio@hotmail.com','4523-1208');
