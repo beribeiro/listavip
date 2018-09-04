f17dda6611650cb05b2650feea14a0c25bef26d5
Bruno <brunoesperancim@hotmail.com>
2018-09-04 20:23:56 -0300

    Aula 3

--- src/main/java/br/com/listavip/model/Convidado.java
+++ src/main/java/br/com/listavip/model/Convidado.java
@@ -14,7 +14,16 @@ public class Convidado {
 	private String nome;
 	private String email;
 	private String telefone;
+	public Convidado() {
+		
+	}
 
+	public Convidado(String nome, String email, String telefone) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
 
 	public Long getId() {
 		return id;
